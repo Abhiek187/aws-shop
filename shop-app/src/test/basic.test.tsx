75fd382cfac63df10185c8d2b51bbc3436ae4e75
Abhiek187 <achaudhuri2011@yahoo.com>
2023-06-14 21:33:39 -0400

    Switch from react-test-renderer to react-testing-library

--- shop-app/src/test/basic.test.tsx
+++ shop-app/src/test/basic.test.tsx
@@ -1,22 +1,12 @@
+import { render, screen } from "@testing-library/react";
 import React from "react";
-import renderer from "react-test-renderer";
 import { expect, test } from "vitest";
 
 import App from "../App";
 
-function toJson(component: renderer.ReactTestRenderer) {
-  const result = component.toJSON();
-  expect(result).toBeDefined();
-  expect(result).toBeInstanceOf(Array);
-  return result as renderer.ReactTestRendererJSON;
-}
-
 test("Renders Vite default page", () => {
-  const component = renderer.create(<App />);
-  const tree = toJson(component);
-  expect(tree).toMatchSnapshot();
+  render(<App />);
 
-  const testInstance = component.root;
-  const countButton = testInstance.findByProps({ className: "read-the-docs" });
-  expect(countButton).toBeTruthy();
+  const header: HTMLHeadingElement = screen.getByText("Vite + React");
+  expect(header).toBeInTheDocument();
 });
