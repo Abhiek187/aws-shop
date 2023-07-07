987acb572c729e32211defbfdce27f07a5228afe
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2023-07-06 23:26:58 -0400

    Fix drift check

--- detect-cfn-drift.sh
+++ detect-cfn-drift.sh
@@ -4,12 +4,13 @@ DRIFT_ID=$(aws cloudformation detect-stack-drift --stack-name $STACK_NAME | jq -
 while : ; do
     # Wait until the drift check is complete
     read DRIFT_STATUS DETECT_STATUS < <(echo $(aws cloudformation describe-stack-drift-detection-status --stack-drift-detection-id $DRIFT_ID | jq -r ".StackDriftStatus, .DetectionStatus"))
-    [ DETECT_STATUS == "DETECTION_IN_PROGRESS" ] || break
+    echo "$DETECT_STATUS"
+    [ $DETECT_STATUS == "DETECTION_IN_PROGRESS" ] || break
     sleep 1
 done
 
-if [ DRIFT_STATUS == "DRIFTED" ]; then
-    aws cloudformation describe-stack-resource-drifts --stack-name $STACK_NAME --stack-resource-drift-status-filters DELETED MODIFIED
+if [ $DRIFT_STATUS == "DRIFTED" ]; then
+    aws cloudformation describe-stack-resource-drifts --stack-name $STACK_NAME --stack-resource-drift-status-filters DELETED MODIFIED --no-cli-pager
     echo "The CloudFormation stack has drifted. See details above." && exit 1
 else
     echo "No drift detected."
