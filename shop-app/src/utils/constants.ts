19e4ec4c0dc809acf57736cc193aa2b6e1a5acb3
Abhiek187 <achaudhuri2011@yahoo.com>
2023-08-12 20:56:44 -0400

    Fix the CORS headers for CloudFront & API Gateway

--- shop-app/src/utils/constants.ts
+++ shop-app/src/utils/constants.ts
@@ -1,4 +1,3 @@
 export abstract class Constants {
-  static readonly BASE_URL =
-    "https://vz5vk4kkvh.execute-api.us-east-1.amazonaws.com";
+  static readonly BASE_URL = "https://dovshfcety3as.cloudfront.net";
 }
