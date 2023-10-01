a33c822252a29a4b953c46d299db2cfc003017de
Abhiek187 <achaudhuri2011@yahoo.com>
2023-10-01 18:52:40 -0400

    Rearrange contents in service cards

--- shop-app/src/components/store/ServiceCard.tsx
+++ shop-app/src/components/store/ServiceCard.tsx
@@ -4,8 +4,12 @@ import {
   Typography,
   CardActions,
   Button,
+  Divider,
 } from "@mui/material";
+import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
+import { yellow } from "@mui/material/colors";
 import React from "react";
+
 import AWSService from "../../types/AWSService";
 
 type ServiceProps = {
@@ -16,27 +20,28 @@ const ServiceCard: React.FC<ServiceProps> = ({ service }) => {
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          {service.Id}
-        </Typography>
-        <Typography variant="h5" component="div">
+        <Typography variant="h5" sx={{ textAlign: "center", mb: 1.5 }}>
           {service.Name}
         </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          {service.Description}
-        </Typography>
-        <Typography variant="body2">
-          Category: {service.Category}
-          <br />
-          Price: ${service.Price}
-        </Typography>
-        <Typography>Free Tier: {service.FreeTier}</Typography>
+        <Typography>{service.Description}</Typography>
       </CardContent>
-      <CardActions>
+      <CardActions sx={{ display: "flex", justifyContent: "space-around" }}>
+        <Typography color="text.secondary">{service.Category}</Typography>
         <Button size="small" className="text-teal-600">
-          {service.Unit}
+          ${service.Price} per {service.Unit}
         </Button>
       </CardActions>
+      {service.FreeTier !== null && service.FreeTier !== undefined && (
+        <>
+          <Divider />
+          <CardActions>
+            <AutoAwesomeIcon sx={{ color: yellow[700], mr: 1.5 }} />
+            <Typography sx={{ textAlign: "right" }}>
+              Free Tier: {service.FreeTier} {service.Unit}s
+            </Typography>
+          </CardActions>
+        </>
+      )}
     </Card>
   );
 };
