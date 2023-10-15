e5d577e3b913130a1b8ce93313f94a04638ae299
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2023-10-14 23:00:16 -0400

    Add BrowserRouter to tests

--- shop-app/src/components/layout/TopBar.test.tsx
+++ shop-app/src/components/layout/TopBar.test.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import { describe, expect, it } from "vitest";
 import TopBar from "./TopBar";
 
 describe("TopBar", () => {
   it("should render", () => {
-    render(<TopBar />);
+    render(
+      <BrowserRouter>
+        <TopBar />
+      </BrowserRouter>
+    );
 
     expect(screen.getByText("AWS Shop")).toBeInTheDocument();
   });
