8e357d8a1edfbea82570d3575ee8272ba823c4ab
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2023-11-06 21:12:13 -0500

    Create method to decode JWTs

--- shop-app/src/utils/oauth.ts
+++ shop-app/src/utils/oauth.ts
@@ -1,5 +1,6 @@
 import store from "../store";
 import { appActions } from "../store/appSlice";
+import { TokenPayload } from "../types/TokenPayload";
 import { Constants } from "./constants";
 
 // Convert a binary string to a base64 URL-encoded string
@@ -28,6 +29,25 @@ const sha256 = async (message: string): Promise<ArrayBuffer> => {
   const msgBuffer = new TextEncoder().encode(message);
   // Hash the buffer using SHA-256
   return await crypto.subtle.digest("SHA-256", msgBuffer);
+};
+
+// Get the JSON payload from a JWT (either an ID or access token)
+export const parseJWT = <T extends TokenPayload>(token: string): T => {
+  // Get the payload part of the token
+  const base64Url = token.split(".")[1];
+  // Convert URL-encoded base64 to regular base64
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  // Convert base64 to JSON string
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      // Convert each char to its URL encoding (hex char code padded with 0s)
+      .map((char) => "%" + ("00" + char.charCodeAt(0).toString(16)).slice(-2))
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload) as T;
 };
 
 // Initiate and authorization code flow with PKCE
