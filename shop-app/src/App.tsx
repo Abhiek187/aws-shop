4b5ca2cc470b5f691649b3387440f7bf9c1ad4a6
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2023-11-15 20:56:22 -0500

    Add a profile & 404 page

--- shop-app/src/App.tsx
+++ shop-app/src/App.tsx
@@ -1,13 +1,21 @@
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import {
+  Button,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
 import { teal, yellow } from "@mui/material/colors";
 import { useSelector } from "react-redux";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 
 import TopBar from "./components/app-bar/TopBar";
 import Store from "./components/store/Store";
 import { selectApp } from "./store/appSlice";
 
-function App() {
+const App = () => {
   const { mode } = useSelector(selectApp);
+  const location = useLocation();
 
   const theme = createTheme({
     palette: {
@@ -30,14 +38,50 @@ function App() {
     <ThemeProvider theme={theme}>
       {/* Have the background color match the theme mode */}
       <CssBaseline />
-      <header className="sticky top-0 z-10">
-        <TopBar />
-      </header>
-      <main className="m-2">
-        <Store />
-      </main>
+      <Routes location={location}>
+        <Route
+          path="/"
+          element={
+            <>
+              <header className="sticky top-0 z-10">
+                <TopBar />
+              </header>
+              <main className="m-2">
+                <Store />
+              </main>
+            </>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <>
+              <header>Profile</header>
+              <main>My profile</main>
+            </>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <main className="flex flex-col items-center gap-3">
+              <Typography
+                variant="h4"
+                sx={{ mt: 3, mx: 3, textAlign: "center" }}
+              >
+                Whoops! This page doesn't exist.{" "}
+              </Typography>
+              <Link to="/">
+                <Button variant="contained" color="secondary">
+                  Back to safety
+                </Button>
+              </Link>
+            </main>
+          }
+        />
+      </Routes>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
