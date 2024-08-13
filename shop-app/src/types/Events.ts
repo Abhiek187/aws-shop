3a21936fb2fd3f06053361e6af553cbfb7f8384d
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2024-08-13 19:58:19 -0400

    Remove old interfaces

--- shop-app/src/types/Events.ts
+++ shop-app/src/types/Events.ts
@@ -7,39 +7,14 @@ export type StoreEventData = {
   serviceName: string;
 };
 
-// export interface StoreEvent extends EventRequest {
-//   name: "store";
-//   properties: {
-//     serviceName: string;
-//   };
-// }
-
 export type AppBarEventData = {
   category?: "free" | "trial" | "paid";
   freeTier?: boolean;
   darkMode?: boolean;
 };
 
-// export interface AppBarEvent extends EventRequest {
-//   name: "app-bar";
-//   properties: {
-//     category?: "free" | "trial" | "paid";
-//     freeTier?: boolean;
-//     darkMode?: boolean;
-//   };
-// }
-
 export type ProfileEventData = {
   loggedIn?: boolean;
   viewedProfile?: boolean;
   loggedOut?: boolean;
 };
-
-// export interface ProfileEvent extends EventRequest {
-//   name: "profile";
-//   properties: {
-//     loggedIn?: boolean;
-//     viewedProfile?: boolean;
-//     loggedOut?: boolean;
-//   };
-// }
