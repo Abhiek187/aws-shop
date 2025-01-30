61561ecb0efb9709f663a7fcec184c7a3c9c3645
Abhiek187 <achaudhuri2011@yahoo.com>
2025-01-29 19:39:59 -0500

    Migrate to tailwind v4

--- shop-app/vite.config.ts
+++ shop-app/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     globals: true,
     environment: "jsdom",
