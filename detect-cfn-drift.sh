6e0385bd591b982d254ee6049912fb5bc43d4d01
Abhiek187 <achaudhuri2011@yahoo.com>
2025-02-08 17:54:38 -0500

    Add drift exception for stacks impacted by the CloudFront tag bug

--- detect-cfn-drift.sh
+++ detect-cfn-drift.sh
@@ -18,8 +18,16 @@ if [ $DETECT_STATUS == "DETECTION_FAILED" ]; then
     aws cloudformation describe-stack-drift-detection-status --stack-drift-detection-id $DRIFT_ID
     echo "Failed to detect drift. See details above." && exit 1
 elif [ $DRIFT_STATUS == "DRIFTED" ]; then
-    aws cloudformation describe-stack-resource-drifts --stack-name $STACK_NAME --stack-resource-drift-status-filters DELETED MODIFIED --no-cli-pager
-    echo "The CloudFormation stack has drifted. See details above." && exit 1
+    # There's a bug with CloudFront tags causing drift detection to always fail:
+    # https://github.com/aws-cloudformation/cloudformation-coverage-roadmap/issues/901
+    read IS_DRIFT_BUG < <(echo $(aws cloudformation describe-stack-resource-drifts --stack-name AWS-Shop-Frontend-Stack --stack-resource-drift-status-filters DELETED MODIFIED | jq -r '(.StackResourceDrifts | length == 1) and (.StackResourceDrifts[0].ResourceType == "AWS::CloudFront::Distribution") and (.StackResourceDrifts[0].PropertyDifferences | length == 1) and (.StackResourceDrifts[0].PropertyDifferences[0].PropertyPath == "/Tags")'))
+    
+    if [ $IS_DRIFT_BUG == "true" ]; then
+        echo "No drift detected. False positive found during drift detection."
+    else
+        aws cloudformation describe-stack-resource-drifts --stack-name $STACK_NAME --stack-resource-drift-status-filters DELETED MODIFIED --no-cli-pager
+        echo "The CloudFormation stack has drifted. See details above." && exit 1
+    fi
 else
     echo "No drift detected."
 fi
