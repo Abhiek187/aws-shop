ec124d3d801e289ac1817249dfa0cb10f4423a34
Abhiek187 <achaudhuri2011@yahoo.com>
2026-03-18 18:56:34 -0400

    Override dependencies to support eslint 10 & vite 8

--- shop-app/eslint.config.js
+++ shop-app/eslint.config.js
@@ -58,7 +58,8 @@ export default defineConfig(
     },
     settings: {
       react: {
-        version: "detect",
+        // version: "detect",
+        version: "19.2.4",
       },
     },
   }
--- shop-app/vite.config.ts
+++ shop-app/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
